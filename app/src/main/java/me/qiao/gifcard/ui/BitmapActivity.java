b3467b88ad1323f3dc99d002df1cf3c4c84302e7
Qiaoidea <qiaoidea@sina.com>
2016-05-16 17:20:00 +0800

    update readme

---
+++ app/src/main/java/me/qiao/gifcard/ui/BitmapActivity.java
@@ -0,0 +1,13 @@
+package me.qiao.gifcard.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BitmapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bitmap);
+    }
+}
